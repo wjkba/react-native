464bd8b7d466255f5c1fe86756de65d8e73ca83a
wjkba <63542318+wjkba@users.noreply.github.com>
2025-07-16 12:13:34 +0200

    add event handling

--- project0/App.js
+++ project0/App.js
@@ -1,28 +1,57 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.whiteBox}>
-        <Text style={{ color: "#fff" }}>Witaj świecie 🌍</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={goalInputHandler}
+          style={styles.textInput}
+          placeholder="Your course goal"
+        />
+        <Button onPress={addGoalHandler} title="Add goal" />
       </View>
-      <Text style={{ color: "#fff", marginBottom: 16 }}>Lorem ipsum dolor</Text>
-      <Button title="Click me" />
+      <View style={styles.goalsContainer}>
+        <Text>List of goals..</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#000000",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 64,
+    paddingHorizontal: 16,
   },
-  whiteBox: {
-    marginBottom: 12,
-    borderWidth: 2,
-    borderColor: "#ffff",
-    padding: 8,
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 8,
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    width: "70%",
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    borderColor: "#cccccc",
+  },
+  goalsContainer: {
+    flex: 6,
   },
 });
