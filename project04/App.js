f52c9c96752e1f3ac982922c0ff449d8d77f7dd8
wjkba <63542318+wjkba@users.noreply.github.com>
2025-07-30 11:48:29 +0200

    add useWindowDimensions hook

--- project04/App.js
+++ project04/App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, ImageBackground, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  ImageBackground,
+  View,
+  SafeAreaView,
+  useWindowDimensions,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useFonts } from "expo-font";
 import StartGameScreen from "./screens/StartGameScreen";
@@ -12,6 +18,10 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
   const [guessRounds, setGuessRounds] = useState(0);
+
+  // here we use a hook that gets window dimensions
+  // it's nice to have when we need dynamic dimensions
+  const { width, height } = useWindowDimensions();
 
   // useFonts returns an array where first element is a boolean that indicates whether
   // the fonts have been loaded already or not
@@ -56,6 +66,8 @@ export default function App() {
     );
   }
 
+  const paddingTopDistance = height < 380 ? 32 : 64;
+
   return (
     <LinearGradient colors={["#e0f7fa", "#ffffff"]} style={styles.rootScreen}>
       <ImageBackground
@@ -64,13 +76,15 @@ export default function App() {
         style={{ flex: 1 }}
         imageStyle={styles.backgroundImage}
       >
-        <SafeAreaView style={{ paddingTop: 64, flex: 1 }}>
+        <SafeAreaView style={{ paddingTop: paddingTopDistance, flex: 1 }}>
           {screen}
         </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
 }
+
+// const deviceHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   rootScreen: {
