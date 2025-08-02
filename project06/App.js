994e22acbeec368a6919ac7941b5a1886fabe4b8
wjkba <63542318+wjkba@users.noreply.github.com>
2025-08-02 11:26:15 +0200

    add stack navigator

--- project06/App.js
+++ project06/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <CategoriesScreen />
+      </NavigationContainer>
     </View>
   );
 }
