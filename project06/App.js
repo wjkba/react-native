41049de9743a6ea562c5853bf7913067e9cb359d
wjkba <63542318+wjkba@users.noreply.github.com>
2025-08-02 11:46:41 +0200

    change navigator from Stack to Native Stack

--- project06/App.js
+++ project06/App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 // Stack is an object with two properties
 // every property holds an object that acts as a component
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
